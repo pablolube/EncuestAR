c2b8a2061a9cf8ef875146645c00626a907035b2
Dario Micheli <dariomicheli.9219@gmail.com>
2025-04-26 22:22:21 -0300

    se agrega primera version de streamlit

---
+++ Inicio.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Encuest.AR')
+st.subheader("¿En qué consiste?")
+st.write("**Encuest.AR** consiste en la disponibilización resumida y jerarquizada de la información contenida en datasets de la encuesta permanente de hogares facilitada por el estado nacional.")
+st.subheader("¿Qué es la EPH?")
+st.write("La **EPH** es un programa nacional de producción permanente de indicadores sociales cuyo objetivo es conocer las características socioeconómicas de la población. Es realizada en forma conjunta por el Instituto Nacional de Estadísticas y Censos (INDEC) y las Direcciones provinciales de estadísticas (DPE). Por trimestre se releva y publica cada encuesta")
---
+++ pages/01_Carga de Datos.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("El sistema contiene información desde el 01/2016 hasta el 03/2024")
+
+st.button("Actualizar datos")
