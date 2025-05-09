9ce57d14cd9c4bcb559f0abf357b8b4fa1bf16fe
Delfina Gonzalez <dauphineg2024@gmail.com>
2025-05-09 11:20:50 -0300

    Arreglo estilos page Carga ST

--- Inicio.py
+++ Inicio.py
@@ -15,17 +15,15 @@ st.markdown('<h3 > Proyecto colectivo: Trabajo integrador UNLP </h3>', unsafe_al
 st.markdown('<hr style="border: 1px solid #dddddd;">', unsafe_allow_html=True)
 
 # Sección: ¿En qué consiste?
-st.markdown('<h3><i class="fas fa-info-circle" style="color:#E67E22;"></i> ¿En qué consiste?</h3>', unsafe_allow_html=True)
-st.markdown("""
-            <div style="text-align: justify;"><strong>Encuest.AR</strong> es una aplicación diseñada para cargar, visualizar y obtener información resumida y jerarquizada. Utilizamos sets de datos brindados por la  <a href="https://www.indec.gob.ar/ftp/cuadros/sociedad/Gacetilla_EPHContinua.pdf" target="_blank"><strong>EPH</strong></a> (Encuesta Permanente de Hogares), los cuales son facilitados de forma pública por el Estado Nacional.
-</div>
-""", unsafe_allow_html=True)
-
-st.markdown("&nbsp;", unsafe_allow_html=True)
+st.markdown('<h3><i class="fa-arrow-down" style="color:#E67E22;"></i> ¿En qué consiste?</h3>', unsafe_allow_html=True)
+st.markdown("""<p
+            style="text-align: justify;"><strong>Encuest.AR</strong> es una aplicación diseñada para cargar, visualizar y obtener información resumida y jerarquizada. Utilizamos sets de datos brindados por la  <a href="https://www.indec.gob.ar/ftp/cuadros/sociedad/Gacetilla_EPHContinua.pdf" target="_blank"><strong>EPH</strong></a> (Encuesta Permanente de Hogares), los cuales son facilitados de forma pública por el Estado Nacional.
+</p>
+""" , unsafe_allow_html=True)
 
 # Link archivo descargable
-st.markdown("""
-<div style="text-align: justify;">
+st.markdown("""<div style="text-align: justify;">
+    <i class="fas fa-arrow-right" style="color:#CA6F1E; margin-right: 8px;"></i>
     <strong>Podés acceder a los archivos descargables en:</strong>
     <a href="https://www.indec.gob.ar/indec/web/Institucional-Indec-BasesDeDatos" target="_blank">
         <strong>Descargar Set de Datos</strong>
@@ -38,13 +36,13 @@ st.markdown('<hr style="border: 1px solid #dddddd;">', unsafe_allow_html=True)
 
 # Sección: ¿Qué es la EPH?
 st.markdown('<h3><i class="fas fa-users" style="color:#CA6F1E;"></i> ¿Qué es la EPH?</h3>', unsafe_allow_html=True)
-st.markdown("""
-            <div style="text-align: justify;">La <strong>EPH</strong> es un programa nacional de producción permanente de indicadores sociales con actualizaciones <strong>trimestrales</strong>  cuyo objetivo es conocer las características socioeconómicas de la población.
+st.markdown("""<p
+            style="text-align: justify;">La <strong>EPH</strong> es un programa nacional de producción permanente de indicadores sociales con actualizaciones <strong>trimestrales</strong>  cuyo objetivo es conocer las características socioeconómicas de la población.
             
 Es realizada en forma conjunta por el Instituto Nacional de Estadística y Censos (<strong>INDEC</strong>) y las Direcciones Provinciales de Estadísticas (<strong>DPE</strong>).
 
 Los datos recolectados son fundamentales para el diseño y monitoreo de políticas públicas, análisis de pobreza, empleo, ingresos, y otros indicadores clave para el desarrollo social y económico del país.    
-</div>
+</p>
 """, unsafe_allow_html=True)
 
 st.video("https://www.youtube.com/watch?v=cQXHWMnaY2A")
