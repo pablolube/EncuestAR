2cea427178993cb3a00ee08eb2fd5bb6ec697d14
Dario Micheli <dariomicheli.9219@gmail.com>
2025-05-23 20:57:05 -0300

    se agregan nuevas pages

--- pages/02_Busqueda por Tema.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Busqueda por Tema")
-st.write("Esta sección está en desarrollo.")
--- pages/03_Visualización.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Visualización de Datos")
-st.write("Esta sección está en desarrollo.")
