4576ee185c3b787de1d9969c40817e649a62310b
Esteeven Andres <gallegoses2000@gmail.com>
2025-06-17 15:50:15 -0300

    Correcion en calculo de promedio de canasta

--- pages/06_Ingresos.py
+++ pages/06_Ingresos.py
@@ -34,14 +34,12 @@ def calculo_promedio_canasta_trimestre(trimestre_ingresado, anio_ingresado, ruta
     
     df_canasta_basica = pd.read_csv(ruta)
 
-    if trimestre_ingresado not in TRIMESTRES:
-        raise ValueError("Trimestre inválido. Debe ser 1, 2, 3 o 4.")
     meses_trimestre = TRIMESTRES[trimestre_ingresado]
     
     df_canasta_basica['indice_tiempo'] = pd.to_datetime(df_canasta_basica.indice_tiempo)
     df_filtrado = df_canasta_basica[(df_canasta_basica.indice_tiempo.dt.year == anio_ingresado) & (df_canasta_basica.indice_tiempo.dt.month.isin(meses_trimestre))]
     
-    if df_filtrado.empty():
+    if df_filtrado.empty:
         raise ValueError(f"No hay datos para el trimestre {trimestre_ingresado} del año {anio_ingresado}.")
 
     prom_indigencia = round(df_filtrado['linea_indigencia'].mean(), 2)
@@ -53,7 +51,7 @@ def calculo_promedio_canasta_trimestre(trimestre_ingresado, anio_ingresado, ruta
     }
 
 def extraer_anios_trimestres_hogares():
-    """Extrae combinaciones (año, trimestre) de nombres de los archivos disponibles."""
+    """Extrae combinaciones (año, trimestre) de los archivos disponibles."""
     
     archivos_hogar = []
     
@@ -95,16 +93,19 @@ if 'df_ind' in st.session_state and not st.session_state.df_ind.empty:
     opciones_disponibles = extraer_anios_trimestres_hogares()
     if not opciones_disponibles:
         st.warning("No se encontraron archivos válidos con información de año y trimestre.")
+    else:   
+        # Uso format_func para que se muestre en un formato mas claro 
+        seleccion = st.selectbox("📅 Seleccioná un período disponible (año y trimestre):", opciones_disponibles, format_func=lambda x: f"{x[0]} - Trimestre {x[1]}") 
         
-    # Uso format_func para que se muestre en un formato mas claro 
-    seleccion = st.selectbox("📅 Seleccioná un período disponible (año y trimestre):", opciones_disponibles, format_func=lambda x: f"{x[0]} - Trimestre {x[1]}") 
+        anio, trimestre = seleccion
+        
+        st.session_state["anio_P7"] = anio
+        st.session_state["trimestre_P7"] = trimestre
+
+
+        Promedio_canasta = calculo_promedio_canasta_trimestre(int(trimestre), int(anio), ruta_archivo)
+        st.write(Promedio_canasta)
     
-    anio, trimestre = seleccion
-    
-    st.session_state["anio"] = anio
-    st.session_state["trimestre"] = trimestre
-    
-    st.success(f"Seleccionaste: Año {anio}, Trimestre {trimestre}")
 #--------Si no existen datos cargados------------------------------------------------------------------
 else:
     st.markdown(
