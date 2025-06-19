22c44e8e0f4c6d3e8aabf165292be8daf6547bff
Esteeven Andres <gallegoses2000@gmail.com>
2025-06-18 22:27:36 -0300

    Introduccion de graficas en P7 con un selectbox y correciones menores

--- pages/06_Ingresos.py
+++ pages/06_Ingresos.py
@@ -67,43 +67,51 @@ def cantidad_porcentaje_pobreza(df_hogares, anio, trimestre, promedio_canasta_ac
     # Eliminar CODUSU duplicados
     df_filtrado = df_filtrado.drop_duplicates(subset='CODUSU', keep='first')
     
+    # Eliminar filas con ITF nulo antes de hacer filtros de pobreza/indigencia
+    df_filtrado = df_filtrado.dropna(subset=['ITF'])
+    df_filtrado = df_filtrado[df_filtrado['ITF'] > 0]
+    
     hogares_totales = df_filtrado['PONDERA'].sum()
 
+    # Clasificaciones
     filtro_pobreza = ((df_filtrado['ITF'] <= promedio_canasta_actual['linea_pobreza']) &
-                      (df_filtrado['ITF'] > promedio_canasta_actual['linea_indigencia'])
-                    )
+                      (df_filtrado['ITF'] > promedio_canasta_actual['linea_indigencia']))
+
+    filtro_indigencia = df_filtrado['ITF'] <= promedio_canasta_actual['linea_indigencia']
+    filtro_no_pobres = df_filtrado['ITF'] > promedio_canasta_actual['linea_pobreza']
+
     hogares_pobreza = df_filtrado[filtro_pobreza]['PONDERA'].sum()
-    hogares_indigencia = df_filtrado[df_filtrado['ITF'] <= promedio_canasta_actual['linea_indigencia']]['PONDERA'].sum()
+    hogares_indigencia = df_filtrado[filtro_indigencia]['PONDERA'].sum()
+    hogares_no_pobres = df_filtrado[filtro_no_pobres]['PONDERA'].sum()
 
     # Porcentajes
-    
     porcentaje_pobreza = (hogares_pobreza / hogares_totales) * 100 if hogares_totales else 0
     porcentaje_indigencia = (hogares_indigencia / hogares_totales) * 100 if hogares_totales else 0
+    porcentaje_no_pobres = (hogares_no_pobres / hogares_totales) * 100 if hogares_totales else 0
+
+    # Crear DataFrame final
+    df_resultado = pd.DataFrame([
+        {'Categoria': 'Por encima de la línea de pobreza',
+         'Cantidad': int(round(hogares_no_pobres)),   
+         'Porcentaje': round(porcentaje_no_pobres, 2)},
+        
+        {'Categoria': 'Pobreza',          
+         'Cantidad': int(round(hogares_pobreza)),     
+         'Porcentaje': round(porcentaje_pobreza, 2)},
+        
+        {'Categoria': 'Indigencia',       
+         'Cantidad': int(round(hogares_indigencia)),  
+         'Porcentaje': round(porcentaje_indigencia, 2)}
+    ])
     
-    resultado = {
-        'hogares_totales': {
-            'cantidad': int(hogares_totales),
-            'porcentaje': 100.0
-        },
-        'pobreza': {
-            'cantidad': int(hogares_pobreza),
-            'porcentaje': round(float(porcentaje_pobreza),2)
-        },
-        'indigencia': {
-            'cantidad': int(hogares_indigencia),
-            'porcentaje': round(float(porcentaje_indigencia),2)
-        }
-    }
-    
-    return resultado
+    return df_resultado
 
     
 #--------------STREAMLIT-------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------
 
 st.set_page_config(page_title='Ingresos', layout="wide")
-st.title('Ingresos')
-st.markdown("Análisis basado en datos de la EPH: Pobreza e indigencia")
+st.title('💰 Ingresos')
 st.markdown('---')
 
 # --- Verificar datos cargados ---
@@ -120,6 +128,8 @@ if 'df_hogares' in st.session_state and not st.session_state.df_hogares.empty:
         seleccion = st.selectbox("📅 Seleccioná un período disponible (año y trimestre):", opciones_disponibles, format_func=lambda x: f"{x[0]} - Trimestre {x[1]}") 
         
         anio, trimestre = seleccion
+
+        st.info(f"""Para el **año {anio} y trimestre {trimestre}**, se presenta la cantidad y porcentaje de **hogares de 4 integrantes** con ingresos bajo la linea de pobreza e indigencia, con base en la Encuesta Permanente de Hogares (EPH).""")
         
         st.session_state["anio_P7"] = anio
         st.session_state["trimestre_P7"] = trimestre
@@ -128,11 +138,51 @@ if 'df_hogares' in st.session_state and not st.session_state.df_hogares.empty:
             int(trimestre), int(anio), RUTA_ARCHIVO_CANASTA
         )
         st.session_state.promedio_canasta = promedio_canasta
-        st.write(promedio_canasta)     
-           
-        dic_total = cantidad_porcentaje_pobreza(df_hg, anio, trimestre, promedio_canasta)
+
+        df = cantidad_porcentaje_pobreza(df_hg, anio, trimestre, promedio_canasta)
+
+        # Selector de tipo de gráfico
+        tipo_grafico = st.segmented_control(label="Seleccioná el tipo de gráfico", options=["Torta", "Barras"], selection_mode='single')
         
-        st.markdown(dic_total)
+        # Muestra de la tabla generada
+        st.dataframe(df)
+
+        # Muestra del grafico seleccionado
+        if tipo_grafico == 'Torta':
+            #  gráfico de torta
+            figura, ax = plt.subplots()
+            ax.pie(
+                df['Porcentaje'], labels=df['Categoria'], 
+                autopct='%1.1f%%', 
+                startangle=90, 
+                colors=['#4CAF50', '#FFC107', '#F44336'] 
+            )
+            ax.axis('equal')  
+            ax.set_title("Distribución de hogares según situación económica")
+
+            # Mostrar gráfico en Streamlit
+            st.pyplot(figura)
+                          
+        elif tipo_grafico == 'Barras':
+            # Barras
+            fig, ax = plt.subplots()   
+            
+            ax.bar(
+                df['Categoria'], 
+                df['Cantidad'],
+                color=['#4CAF50', '#FFC107', '#F44336'],
+                width=0.4
+            )
+            
+            ax.set_xlabel('Situacion Economica')   
+            ax.set_ylabel('Cantidad de Hogares')
+            ax.set_title('Distribución de hogares de 4 integrantes según situación económica')
+
+            st.pyplot(fig) 
+        st.markdown("---")
+        st.caption("📊 Fuente: Encuesta Permanente de Hogares (EPH) - INDEC")
+
+
 #--------Si no existen datos cargados------------------------------------------------------------------
 else:
     st.markdown(
